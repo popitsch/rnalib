af1ec71a33d0878106c31cdc298ee66908663b6c
popitsch <niko.popitsch@univie.ac.at>
2024-01-24 15:31:27 +0100

    added pypi requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rnalib',
+    version='1.0.0',
+    packages=['tests', 'rnalib'],
+    url='https://github.com/popitsch/rnalib',
+    license='Apache License v2.0',
+    author='niko',
+    author_email='niko.popitsch@univie.ac.at',
+    description='a python-based transcriptomics library'
+)
